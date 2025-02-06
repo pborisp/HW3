6aa8ec5ce9ae0d474ea50f04fa0c9ce2b54bfad1
Slava ASUS <popovbs@gmail.com>
2025-02-07 01:37:17 +0300

    Tasks 1,2

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,23 @@
 public class Main {
     public static void main(String[] args) {
+        // Задание №1
+        System.out.println("Задача 1:");
         int age = 10;
         if (age >= 18) {
             System.out.println("Человеку "+age+" лет. Он совершеннолетний.");
         } else {
             System.out.println("Человеку "+age+" лет. Он не достиг совершеннолетия, нужно немного подождать.");
         }
+        System.out.println();
+
+        // Задача №2
+        System.out.println("Задача 2:");
+        int temp = 4;
+        if (temp < 5) {
+            System.out.println("На улице " + temp + " градусов. Нужно надеть шапку.");
+        } else {
+            System.out.println("На улице " + temp + " градусов. Можно идти без шапки.");
+        }
+        System.out.println();
     }
 }
